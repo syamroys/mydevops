fdc2e1c88c65ca5cd3ce3094958c832e3ec56e7d
syamroys <emandhishyam1234@gmail.com>
2023-06-28 11:04:03 +0530

    this is my day 2 commit

---
+++ Second.java
@@ -0,0 +1,8 @@
+class Second
+{
+    public static void main(String args[])
+    {
+       System.out.println("this is syam");
+    }
+}
+    
---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sum 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your number");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("sum is"+(a+b));
+
+    }
+}
